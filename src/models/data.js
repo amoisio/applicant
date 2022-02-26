386ff42a62e16803616c4439be6ae4e07ef66009
Aleksi Moisio <aleksi.moisio30@gmail.com>
2022-02-27 00:06:15 +0200

    Add mock data and models

---
+++ src/models/data.js
@@ -0,0 +1,8 @@
+import Template from './template';
+
+export default class Data {
+  constructor() {
+    this.template = new Template();
+    this.entries = [];
+  }
+}
---
+++ src/models/entry.js
@@ -0,0 +1,8 @@
+export default class Entry {
+  constructor(title) {
+    this.title = title;
+    this.started = null;
+    this.finished = null;
+    this.answers = [];
+  }
+}
