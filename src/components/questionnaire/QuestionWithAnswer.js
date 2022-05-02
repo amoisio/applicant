9ff4b14ac4794271d3ad5be32753d17b1023f39b
Aleksi Moisio <aleksi.moisio30@gmail.com>
2022-05-02 10:56:33 +0300

    Refactor question with answer component

--- src/components/questionnaire/QuestionWithAnswer.js
+++ src/components/questionnaire/QuestionWithAnswer.js
@@ -1,22 +1,16 @@
 import { trimmedOrDefault } from '../../models/common';
-import QuestionText from './QuestionText';
 import AutoGrowTextarea from '../shared/AutoGrowTextarea';
+
 /**
  * Questionnaire question with an answer component for answering a questionnaire question.
  * @param {string} question Question text.
  * @param {string} answer Answer text.
  * @param {function} onChange onChange(modifiedAnswer: string) callback called when answer text changes.
  */
-export default function QuestionWithAnswer(props) {
-  const onChange = props.onChange;
-  if (!onChange) {
-    throw new Error('onChange callback not defined.');
-  }
-  const question = trimmedOrDefault(props.question);
-  if (!question) {
-    throw new Error('Non-empty question must be given.');
-  }
-  const answer = props.answer;
+export default function QuestionWithAnswer({ question, answer, onChange }) {
+  const questionText = trimmedOrDefault(question);
+  if (!questionText)  throw new Error('Non-empty question must be given.');
+  if (!onChange)      throw new Error('onChange callback not defined.');
 
   const checkmark =
     answer && answer.length > 0 ? (
@@ -31,12 +25,11 @@ export default function QuestionWithAnswer(props) {
 
   return (
     <div className='questionnaire-question'>
-      <QuestionText question={question}></QuestionText>
+      <p>{questionText}</p>
       <AutoGrowTextarea
         placeholder='Enter answer text...'
         onChange={onChange}
-        value={answer}
-      />
+        value={answer} />
       {checkmark}
     </div>
   );
