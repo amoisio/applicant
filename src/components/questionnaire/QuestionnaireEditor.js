75e162cd86dd6127eb94d70a7268f643edee625e
Aleksi Moisio <aleksi.moisio30@gmail.com>
2022-05-02 11:24:04 +0300

    Refactor questionnaire editor

--- src/components/questionnaire/QuestionnaireEditor.js
+++ src/components/questionnaire/QuestionnaireEditor.js
@@ -4,28 +4,17 @@ import QuestionWithAnswer from './QuestionWithAnswer';
  * Questionnaire editor component
  * @param {object} questionnaire Questionnaire.
  * @param {function} onAnswer onAnswer(id: string, modifiedAnswer: string) callback function called when changing answer text.
- * @param {function} onComplete onComplete(id: string) callback function called when completing the questionnaire.
  */
-export default function QuestionnaireEditor(props) {
-  const questionnaire = props.questionnaire;
-  if (!questionnaire) {
-    throw new Error('Questionnaire must be given.');
-  }
-  const onAnswer = props.onAnswer;
-  if (!onAnswer) {
-    throw new Error('onAnswer callback not given.');
-  }
-  const questions = questionnaire.questions;
-  const answers = questions.map((question) => {
+export default function QuestionnaireEditor({ questionnaire, onAnswer }) {
+  if (!questionnaire) throw new Error('Questionnaire must be given.');
+  if (!onAnswer)      throw new Error('onAnswer callback not given.');
+  const answers = questionnaire.questions.map((question) => {
     return (
       <QuestionWithAnswer
         key={question.id}
         question={question.text}
         answer={question.answer}
-        onChange={(modifiedAnswer) =>
-          onAnswer(question.id, modifiedAnswer)
-        }
-      />
+        onChange={(modifiedAnswer) => onAnswer(question.id, modifiedAnswer)} />
     );
   });
   return (
